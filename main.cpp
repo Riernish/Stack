c2e44126d6cca7b7e590c30462b3be1b1e25d94a
Riernish <osipov.ia@phystech.edu>
2019-11-23 16:51:16 +0300

    example of not created stack's work

--- main.cpp
+++ main.cpp
@@ -8,20 +8,14 @@ const int NUM = 17;
 int main() {
 
     stack_t<int> stk;
-    stk.construct(0);
-    stack_t<char> stk2;
-    stk2.construct(0);
-    stack_t <unsigned long long> u_stk;
-    u_stk.construct(0);
+    //stk.construct(0);
     for (int i = 0; i < NUM; i -= -1) {
 
-        stk.push(i+5);
-        stk2.push(i + 50);
-        u_stk.push(i);
+        stk.push(-i);
 
     }
     for (int i = 0; i < NUM; i++)
-        cout << hex << stk.pop()<< ' ' << stk2.pop() << endl;
+        cout << hex << stk.pop()<< ' ' << endl;
     stk.pop();
 
     stk.destruct();
